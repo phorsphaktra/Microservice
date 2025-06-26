dfa707d3ddf6f529d20e277ab6ad2807ed449e90
Phaktra MacBook Pro <phaktra@Phaktras-MacBook-Pro.local>
2025-06-26 16:34:54 +0700

    part 3

---
+++ api-gateway/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.phaktra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
