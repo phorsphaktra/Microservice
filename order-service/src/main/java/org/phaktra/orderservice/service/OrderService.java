7dd12442f35f82fe02e9d7615a2bc296c8a224b5
Phaktra MacBook Pro <phaktra@Phaktras-MacBook-Pro.local>
2025-06-26 15:52:42 +0700

    done

--- order-service/src/main/java/org/phaktra/orderservice/service/OrderService.java
+++ order-service/src/main/java/org/phaktra/orderservice/service/OrderService.java
@@ -19,7 +19,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
 
     public void placeOrder(OrderRequest orderRequest) {
@@ -39,8 +39,8 @@ public class OrderService {
                 .toList();
 
         //todo Call Inventory Service, and place order if product is in stock
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCode).build())
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCode).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
                 .block();
